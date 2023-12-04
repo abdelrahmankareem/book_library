812dd689384b0c35a0ff38d4d846610880878899
abdelrahmankareem <abdoluka@gmail.com>
2023-12-04 23:17:39 +0200

    adding animation to splash view

--- lib/features/splash/presentations/views/widgets/splash_body.dart
+++ lib/features/splash/presentations/views/widgets/splash_body.dart
@@ -1,8 +1,28 @@
 import 'package:book/core/utils/assets.dart';
+import 'package:book/features/splash/presentations/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashBody extends StatelessWidget {
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,11 +31,7 @@ class SplashBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          "Welcome To your Dreams",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        )
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
