eb948d7c42dcfb8c3e848310a7d09efdfa217e6a
abdelrahmankareem <abdoluka@gmail.com>
2024-01-03 21:10:36 +0200

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => AuthCubit(),
       child: MaterialApp.router(
+        theme: ThemeData(textTheme: GoogleFonts.montserratTextTheme()),
         routerConfig: AppRouter.router,
         debugShowCheckedModeBanner: false,
       ),
