2c0d8ff9c700f67560ec8cc10a41fac7974b52c2
abdelrahmankareem <abdoluka@gmail.com>
2024-01-09 00:15:59 +0200

    handling exception scroll book details

--- lib/features/home/presentations/views/widget/books_details_view_body.dart
+++ lib/features/home/presentations/views/widget/books_details_view_body.dart
@@ -12,55 +12,67 @@ class BooksDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(children: [
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: width * .28),
-        child: const CustomBookImage(),
-      ),
-      const SizedBox(
-        height: 45,
-      ),
-      const Text(
-        "The Jungle Book",
-        style: Styles.textStyle30,
-      ),
-      const SizedBox(
-        height: 8,
-      ),
-      Opacity(
-        opacity: .7,
-        child: Text(
-          "abdo kareem",
-          style: Styles.textStyle18.copyWith(
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-      const SizedBox(
-        height: 17,
-      ),
-      const BookRating(
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-      const SizedBox(
-        height: 37,
-      ),
-      const BooksAction(),
-      const SizedBox(
-        height: 50,
-      ),
-      Padding(
-        padding: const EdgeInsets.only(left: 30),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "You can also like ",
-            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-      const SimilarbookslistView(),
-    ]);
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * .28),
+                child: const CustomBookImage(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                "The Jungle Book",
+                style: Styles.textStyle30,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text(
+                  "abdo kareem",
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 17,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 37,
+              ),
+              const BooksAction(),
+              const Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You can also like ",
+                    style: Styles.textStyle14
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SimilarbookslistView(),
+              const SizedBox(
+                height: 40,
+              ),
+            ])),
+      ],
+    );
   }
 }
